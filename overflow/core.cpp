a638abbacfe8e74fe79d4f864dc5d8a130ad1068
star._.overflow <2020320120@korea.ac.kr>
2022-04-14 10:42:55 +0900

    fix problem with Shape::T()

--- overflow/core.cpp
+++ overflow/core.cpp
@@ -2,41 +2,63 @@
 
 // Shape ##################################################
 
-Shape::Shape() {
+Shape::Shape()
+{
+    std::cout << "Shape::Shape()" << std::endl;
     shape = NULL;
     dim = 0;
     size = 0;
 }
 
-Shape::Shape(int *shape_, int dim_) {
+Shape::Shape(const int *shape_, const int dim_)
+{
     dim = dim_;
     size = 1;
-    shape = new int(dim);
-    for (int i = 0; i < dim; i++) {
+    shape = new int[dim];
+    for (int i = 0; i < dim; i++)
+    {
         shape[i] = shape_[i];
         size *= shape[i];
     }
 }
 
-void Shape::T() {
-    int *shape_ = new int(dim);
+void Shape::init(const int *shape_, const int dim_)
+{
+    dim = dim_;
+    size = 1;
+    shape = new int[dim];
+    for (int i = 0; i < dim; i++)
+    {
+        shape[i] = shape_[i];
+        size *= shape[i];
+    }
+}
 
-    for (int i = 0; i < dim; i++) {
+void Shape::T()
+{
+    int *shape_;
+    shape_ = new int[dim];
+
+    for (int i = 0; i < dim; i++)
+    {
         shape_[i] = shape[i];
     }
 
-    for (int i = 0; i < dim; i++) {
+    for (int i = 0; i < dim; i++)
+    {
         shape[i] = shape_[dim - 1 - i];
     }
 
-    delete shape_;
+    delete[] shape_;
 }
 
-void Shape::print() {
+void Shape::print()
+{
     std::cout << "dim : " << dim << std::endl;
     std::cout << "size : " << size << std::endl;
     std::cout << "shape : [ ";
-    for (int i = 0; i < dim; i++) {
+    for (int i = 0; i < dim; i++)
+    {
         std::cout << shape[i] << " ";
     }
     std::cout << "]" << std::endl;
@@ -44,39 +66,52 @@ void Shape::print() {
 
 // Tensor #################################################
 
-Tensor::Tensor() {
-    tensor_shape = new Shape;
+Tensor::Tensor()
+{
     data = NULL;
 }
 
-Tensor::Tensor(float *data_, int *shape_, int dim_) {
-    tensor_shape = new Shape(shape_, dim_);
+Tensor::Tensor(const double *data_, const int *shape_, const int dim_)
+{
+    tensor_shape.init(shape_, dim_);
 
-    data = new float(tensor_shape->size);
-    for (int i = 0; i < tensor_shape->size; i++) {
+    data = new double[tensor_shape.size];
+    for (int i = 0; i < tensor_shape.size; i++)
+    {
         data[i] = data_[i];
     }
 }
 
-Tensor::Tensor(float data_, int *shape_, int dim_) {
-    tensor_shape = new Shape(shape_, dim_);
+Tensor::Tensor(const double data_, const int *shape_, const int dim_)
+{
+    tensor_shape.init(shape_, dim_);
 
-    data = new float(tensor_shape->size);
-    for (int i = 0; i < tensor_shape->size; i++) {
+    data = new double[tensor_shape.size];
+    for (int i = 0; i < tensor_shape.size; i++)
+    {
         data[i] = data_;
     }
 }
 
-void Tensor::T() { // not generalized
+void Tensor::T()
+{ // not generalized
 
-    tensor_shape->T();
+    tensor_shape.T();
+    std::cout << "void Tensor::T()" << std::endl;
+
+    for (int i = 0; i < tensor_shape.size; i++) {
+        std::cout << data[i] << ' ';
+    }
+    std::cout << std::endl;
 }
 
-void Tensor::print() {
-    tensor_shape->print();
+void Tensor::print()
+{
+    tensor_shape.print();
 
     std::cout << "data : " << std::endl;
-    for (int i = 0; i < tensor_shape->size; i++) {
+    for (int i = 0; i < tensor_shape.size; i++)
+    {
         std::cout << data[i] << " ";
     }
     std::cout << std::endl;
--- overflow/core.h
+++ overflow/core.h
@@ -1,47 +1,44 @@
 #include <iostream>
 
-
 class Shape;
 class Tensor;
 class Function;
-
 
 class Shape
 {
 public:
     Shape();
-    Shape(int *shape_, int dim_);
+    Shape(const int *shape_, const int dim_);
+
+    void init(const int *shape_, const int dim_);
 
     void T();
 
     void print();
-    
+
     // variable
     int *shape;
     int dim;
     int size;
 };
 
-
 class Tensor
 {
 public:
     Tensor();
-    Tensor(float *data_, int *shape_, int dim_);
-    Tensor(float data_, int *shape_, int dim_);
+    Tensor(const double *data_, const int *shape_, const int dim_);
+    Tensor(const double data_, const int *shape_, const int dim_);
 
     void T();
 
     void print();
 
     // variable
-    Shape *tensor_shape;
-    float *data;
+    double *data;
+    Shape tensor_shape;
 };
-
 
 class Function
 {
 public:
-
 };
